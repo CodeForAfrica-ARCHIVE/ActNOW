d4ef33bd5cf33a0b1fb6147ec1a16225036911bb
Nick Hargreaves <hargreaves1234@gmail.com>
2015-12-14 13:44:38 +0300

    create migration for petitions table

---
+++ database/migrations/2015_12_14_103646_create_petitions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('petitions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('hashtag');
+            $table->string('sms_number');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('petitions');
+    }
+}

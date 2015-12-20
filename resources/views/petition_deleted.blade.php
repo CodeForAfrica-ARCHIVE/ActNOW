5ece8184ba7f01ab09039e7093f6be581c220c18
Nick Hargreaves <hargreaves1234@gmail.com>
2015-12-20 12:43:37 +0300

    confirmation message before delete petition

--- resources/views/petition_deleted.blade.php
+++ resources/views/petition_deleted.blade.php
@@ -7,7 +7,7 @@
         <div class="alert alert-danger" role="alert">
             <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
             <span class="sr-only">Error:</span>
-            Petition deleted successfully!
+            Petition deleted successfully! Go to <a href="/dashboard">petitions.</a>
         </div>
     </div>
 @stop
--- resources/views/single_petition.blade.php
+++ resources/views/single_petition.blade.php
@@ -3,6 +3,21 @@
 @section('title', $petition->name)
 
 @section('content')
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            $("#delete-petition").click('click', function (e) {
+                var confirm_delete = confirm("Are you sure you want to delete the petition?");
+                if (confirm_delete) {
+                    //delete petition
+                    $(location).attr('href', "{{ URL::to('petition/delete',array(),false) }}/" + {!! $petition->id !!});
+                }
+            });
+        });
+
+    </script>
+
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <ol class="breadcrumb">
             <li><a href="/dashboard">Petitions</a></li>
@@ -15,11 +30,12 @@
                     <span class="action-button"><i class="fa fa-plus"></i> Edit</span>
                 </button>
             </a>
-            <a href="delete/{!! $petition->id !!}">
+
+            <span id="delete-petition" data-petition="{!! $petition->id !!}">
                 <button type="button" class="btn btn-danger">
                     <span class="action-button"><i class="fa fa-plus"></i> Delete</span>
                 </button>
-            </a>
+            </span>
         </div>
 
         <div class="panel panel-info">

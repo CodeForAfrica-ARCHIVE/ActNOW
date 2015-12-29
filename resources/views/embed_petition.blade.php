f80c9f10cd19dd589ef5259bf8192175df2e0471
Nick Hargreaves <hargreaves1234@gmail.com>
2015-12-29 14:48:21 +0300

    include 'embed petition link' in embed petition frame

--- resources/views/embed_petition.blade.php
+++ resources/views/embed_petition.blade.php
@@ -15,6 +15,27 @@
             </div>
         </div>
 
+        <span class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">Embed this petition</span>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Embed code</h4>
+                    </div>
+                    <div class="modal-body">
+                        Copy and paste the code below where you want the petition to appear.
+                        <textarea class="form-control"><iframe src="{!! URL::to("/embed/" . $petition->id) !!}" scrolling="no" frameborder="0" width="150px" height="100px"></iframe></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </div>
 @stop
